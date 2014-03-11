3f5841a1d0c8657f8e303424ac159fa44cec567e
gentimouton <gentimouton@gmail.com>
2014-03-11 01:16:39 -0700

    multiplayer snake with new network API but without pellets

---
+++ 00-test/snakeclient.py
@@ -0,0 +1,73 @@
+from network import Handler, poll
+from pygame.locals import KEYDOWN, QUIT, K_ESCAPE, K_UP, K_DOWN, K_LEFT, K_RIGHT
+from random import randint
+import pygame
+
+
+myname = randint(0, 10 ** 9)  # avoid name collisions
+other_players = {}  # map names to rectangles
+
+class Client(Handler):
+    
+    def on_open(self):
+        self.do_send({'msgtype':'join', 'name':myname})
+    
+    def on_close(self):
+        pass
+    
+    def on_msg(self, data):
+        msgtype = data['msgtype']
+        name = data['name']
+        if msgtype == 'join': # someone joined
+            if name != myname: # ignore messages about me
+                other_players[name] = pygame.Rect(200, 150, 10, 10)
+        elif msgtype == 'leave': # someone left
+            del other_players[name]
+        elif msgtype == 'move': # someone moved
+            if name != myname: # ignore messages about me
+                top, left = data['top'], data['left']
+                other_players[name] = pygame.Rect(left, top, 10, 10)
+
+client = Client('localhost', 8888)
+
+pygame.init()
+screen = pygame.display.set_mode((400, 300))
+screen.fill((0, 0, 0))  # black
+
+myrect = pygame.Rect(200, 150, 10, 10)  # start position: middle of the screen
+dx, dy = 0, 1  # starting direction: down
+
+clock = pygame.time.Clock()
+
+while True:
+    clock.tick(20)
+    poll() # push and pull network messages
+    
+    for event in pygame.event.get():  # keyboard inputs
+        if (event.type == QUIT):
+            exit()
+        if event.type == KEYDOWN:
+            key = event.key
+            if key == K_ESCAPE:
+                exit()
+            elif key == K_UP:
+                dx, dy = 0, -1
+            elif key == K_DOWN:
+                dx, dy = 0, 1
+            elif key == K_LEFT:
+                dx, dy = -1, 0
+            elif key == K_RIGHT:
+                dx, dy = 1, 0
+        
+    if not client.connected:  # display only after we connect
+        continue
+    
+    myrect.move_ip(dx, dy) # update my game state and forward it to others
+    client.do_send({'msgtype': 'move', 'name': myname,
+                    'top': myrect.top, 'left': myrect.left})
+    
+    screen.fill((0, 0, 0))  # black
+    pygame.draw.rect(screen, (255, 0, 0), myrect) # red
+    for rect in other_players.values():
+        pygame.draw.rect(screen, (255, 255, 255), rect) # white
+    pygame.display.update()
---
+++ 00-test/snakeserver.py
@@ -0,0 +1,39 @@
+from network import Listener, Handler, poll
+from time import sleep
+
+
+handlers = set()
+def broadcast(msg):
+    copies = handlers.copy() # avoid "Set changed size during iteration" error
+    [h.do_send(msg) for h in copies]
+    
+    
+class MyHandler(Handler):
+    
+    def on_open(self):
+        self.myname = ''
+        handlers.add(self)
+        
+    def on_close(self):
+        handlers.remove(self)
+        broadcast({'msgtype': 'leave', 'name': self.myname})
+        
+    def on_msg(self, data):
+        msgtype = data['msgtype']
+        name = data['name']
+        if msgtype == 'join': # the player joined: forward his/her name
+            self.myname = name
+            broadcast({'msgtype': 'join', 'name': name})
+        elif msgtype == 'move':
+            top, left = data['top'], data['left']
+            broadcast({'msgtype': 'move', 'name': name,
+                       'top': top, 'left': left})
+            
+            
+class Server(Listener):
+    handlerClass = MyHandler
+
+server = Server(8888)
+while 1:
+    poll()
+    sleep(.1)
