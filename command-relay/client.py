6eecf60307beadfe25467cf20565d0a6a7129541
tho deb <gentimouton@gmail.com>
2014-03-13 17:17:25 -0700

    big whales eat small whales

--- command-relay/client.py
+++ command-relay/client.py
@@ -12,11 +12,17 @@ from network import Handler, poll
 myname, mybox = None, None  # myname is given by the server
 players = {}  # map player name to rectangle
 pellets = []
+
+class Player(Rect):
+    def __init__(self, l, t, w, h, name):
+        self.name = name
+        Rect.__init__(self, l, t, w, h)
         
 class Client(Handler):
             
     def on_msg(self, data):
         msgtype, name = data['msg_type'], data['name']
+        global myname, mybox
         
         if msgtype == 'welcome':
             for state in data['pellets']:  # create all pellets
@@ -24,24 +30,32 @@ class Client(Handler):
                 pellets.append(Rect(left, top, width, height))
             for pname, state in data['players'].items():  # create all players
                 left, top, width, height = tuple(state)
-                players[pname] = Rect(left, top, width, height)
-            global myname, mybox
+                players[pname] = Player(left, top, width, height, pname)
             myname, mybox = name, players[name] 
                 
         elif msgtype == 'join' and name != myname:  # ignore my join message
             left, top, width, height = tuple(data['state']) 
-            players[name] = Rect(left, top, width, height)
+            players[name] = Player(left, top, width, height, name)
             
         elif msgtype == 'leave':
             del players[name]
             
         elif msgtype == 'move' and name != myname:  # ignore my move messages
             left, top, width, height = tuple(data['state'])
-            players[name] = Rect(left, top, width, height)
+            players[name] = Player(left, top, width, height, name)
             
-        elif msgtype == 'eat':
+        elif msgtype == 'eat_pellet':
             players[name].size = tuple(data['size'])  
             pellets[data['pellet_index']] = data['new_pellet']
+        
+        elif msgtype == 'grow':
+            players[name].size = tuple(data['size'])  
+        
+        elif msgtype == 'die':
+            left, top, width, height = tuple(data['state'])
+            players[name] = Player(left, top, width, height, name)
+            if name == myname:
+                mybox = players[myname]
             
             
 client = Client('localhost', 8888)
@@ -81,25 +95,41 @@ while True:
     
     delay -= 4
     
-#     if mybox.collidelist(borders) != -1:  # game over
-#         mybox = Rect(200, 150, 10, 10)  # back to middle of the screen
-#         delay = 0  # move right away
+    if mybox and mybox.collidelist(borders) != -1:  # game over
+         client.do_send({'msg_type': 'die'})
+         mybox = None
+         delay = 0  # move right away
 
-    if delay <= 0:
-        mybox.move_ip(dx, dy)  # update position
-        delay = (mybox.width - 10) / 2  # number of pellets eaten so far
-        client.do_send({'msg_type': 'move',
-                        'state': [mybox.left, mybox.top, mybox.w, mybox.h]})
+    if mybox:
+        if delay <= 0:  # time to move
+            mybox.move_ip(dx, dy)  # update position
+            delay = (mybox.width - 10) / 2  # number of pellets eaten so far
+            client.do_send({'msg_type': 'move',
+                            'state': [mybox.left, mybox.top, mybox.w, mybox.h]})
         
-    p_index = mybox.collidelist(pellets)
-    if p_index != -1:  # ate a pellet: grow, and replace a pellet
-        client.do_send({'msg_type': 'eat',
-                        'pellet_index': p_index})
+        player_list = players.values()
+        pl_idx = mybox.collidelist(player_list)
+        if pl_idx != -1 and player_list[pl_idx].name != myname:  # collide with other player
+            hisbox = player_list[pl_idx]
+            if hisbox.width >= mybox.width:  # die if smaller
+                mybox = None
+                delay = 0
+                client.do_send({'msg_type': 'die'})
+            else:  # tell server if bigger
+                client.do_send({'msg_type': 'eat_player',
+                                'eaten': [hisbox.w, hisbox.h]})
+    
+    if mybox: # may have been Noned by colliding with another player        
+        pe_idx = mybox.collidelist(pellets)
+        if pe_idx != -1:  # ate a pellet: grow, and replace a pellet
+            client.do_send({'msg_type': 'eat_pellet',
+                            'pellet_index': pe_idx})
         
     
     screen.fill((0, 0, 64))  # dark blue
     [draw_rect(screen, (0, 191, 255), b) for b in borders]  # red
     [draw_rect(screen, (255, 192, 203), p) for p in pellets]  # shrimp
-    [draw_rect(screen, (255, 0, 0), p) for p in players.values()]  # red
-    draw_rect(screen, (0, 191, 255), mybox)  # Deep Sky Blue
+    [draw_rect(screen, (255, 0, 0), r) for n, r in players.items() if n != myname]  # red
+    if mybox:  # I'm alive
+        draw_rect(screen, (0, 191, 255), mybox)  # Deep Sky Blue
     update_display()
--- command-relay/server.py
+++ command-relay/server.py
@@ -7,14 +7,14 @@ from network import Listener, Handler, poll
 # gameplay 
 pellets = []
 for _ in range(4):  # 4 pellets in the game
-    pellets.append([randint(0, 390), randint(0, 290), 5, 5])
+    pellets.append([randint(10, 390), randint(10, 290), 5, 5])
     
 players = {}  # map name to rectangle
 player_id = 0
 def add_player():
     global player_id
     player_id += 1
-    players[str(player_id)] = [randint(0, 390), randint(0, 290), 10, 10]
+    players[str(player_id)] = [randint(10, 380), randint(10, 280), 10, 10]
     return str(player_id) 
 
 # network 
@@ -50,18 +50,32 @@ class MyHandler(Handler):
                        'name': self.myname,
                        'state': data['state']})
             
-        elif msgtype == 'eat':  # replace pellet, grow player, and forward new game state
+        elif msgtype == 'eat_pellet':  # replace pellet, grow player, and forward new game state
             p_index = data['pellet_index']
-            pellets[p_index] = [randint(0, 390), randint(0, 290), 5, 5]
+            pellets[p_index] = [randint(10, 390), randint(0, 290), 5, 5]
             w, h = players[self.myname][2] * 1.1, players[self.myname][3] * 1.1
             players[self.myname][2] = w
-            players[self.myname][3] = h
-            
-            broadcast({'msg_type': 'eat',
+            players[self.myname][3] = h            
+            broadcast({'msg_type': 'eat_pellet',
                        'name': self.myname,
                        'pellet_index': p_index,
                        'new_pellet': pellets[p_index],
                        'size': [w, h]})
+    
+        elif msgtype == 'eat_player':  # grow by the size of the player eaten 
+            players[self.myname][2] += data['eaten'][0]
+            players[self.myname][3] += data['eaten'][1]
+            broadcast({'msg_type': 'grow',
+                       'name': self.myname,
+                       'size': [players[self.myname][2], players[self.myname][3]]})
+            
+        elif msgtype == 'die':  # back to normal size, and place me randomly
+            players[self.myname] = [randint(10, 380), randint(10, 280), 10, 10]
+            broadcast({'msg_type': 'die',
+                       'name': self.myname,
+                       'state': players[self.myname]})
+        
+        
         
               
 class Server(Listener):
