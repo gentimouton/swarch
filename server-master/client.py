9849088d1b7d3ee7e7b636a1214848de897e4230
tho deb <gentimouton@gmail.com>
2014-03-31 13:18:15 -0700

    almighty server with slave clients working

--- server-master/client.py
+++ server-master/client.py
@@ -1,80 +1,74 @@
-from random import randint
-import time
-
-from pygame import Rect, init as pygame_init, event as pygame_event
-from pygame.display import set_mode, update as update_display
-from pygame.draw import rect as draw_rect
-from pygame.font import Font
-from pygame.locals import KEYDOWN, QUIT, K_ESCAPE, K_UP, K_DOWN, K_LEFT, K_RIGHT, \
-    K_SPACE
-from pygame.time import Clock
-
+"""
+The Client is slave: 
+- it sends only the player inputs to the server.
+- every frame, it displays the server's last received data
+Pros: the server is the only component with game logic, 
+so all clients see the same game at the same time (consistency, no rollbacks).
+Cons: lag between player input and screen display (one round-trip).
+But the client can smooth the lag by interpolating the position of the boxes. 
+"""
 from network import Handler, poll
 
+from pygame import Rect, init as init_pygame
+from pygame.display import set_mode, update as update_pygame_display
+from pygame.draw import rect as draw_rect
+from pygame.event import get as get_pygame_events
+from pygame.locals import KEYDOWN, QUIT, K_ESCAPE, K_UP, K_DOWN, K_LEFT, K_RIGHT
+from pygame.time import Clock
 
-myname, mybox = None, None  # myname is given by the server
-players = {}  # map player name to rectangle
+
+borders = []
 pellets = []
+players = {}  # map player name to rectangle
+myname = None
+     
+init_pygame()
+screen = set_mode((400, 300))
+clock = Clock()
 
+def make_rect(quad):  # make a pygame.Rect from a list of 4 integers
+    x, y, w, h = quad
+    return Rect(x, y, w, h)
+    
 class Client(Handler):
             
     def on_msg(self, data):
-        msgtype = data['msg_type']
-        global myname, mybox
+        global borders, pellets, players, myname
+        borders = [make_rect(b) for b in data['borders']]
+        pellets = [make_rect(p) for p in data['pellets']]
+        players = {name: make_rect(p) for name, p in data['players'].items()}
+        myname = data['myname']
         
-            
-client = Client('localhost', 8888)
+client = Client('localhost', 8888)  # connect asynchronously
 
-pygame_init()
-screen = set_mode((400, 300))
-clock = Clock()
-font = Font(None, 15)  # default pygame Font, height in pixels
+valid_inputs = {K_UP: 'up', K_DOWN: 'down', K_LEFT: 'left', K_RIGHT: 'right'}
 
-borders = [Rect(0, 0, 2, 300), Rect(0, 0, 400, 2),
-           Rect(398, 0, 2, 300), Rect(0, 298, 400, 2)]
-dx, dy = 0, 1  # start direction: down
-delay = 0  # start moving right away 
-
-while True:
+while 1:
     
-    clock.tick(50)  # frames per second
     poll()  # push and pull network messages
 
-    for event in pygame_event.get():  # inputs
+    # send valid inputs to the server
+    for event in get_pygame_events():  
         if event.type == QUIT:
             exit()
         if event.type == KEYDOWN:
             key = event.key
             if key == K_ESCAPE:
                 exit()
-            elif key == K_UP:
-                dx, dy = 0, -1
-            elif key == K_DOWN:
-                dx, dy = 0, 1
-            elif key == K_LEFT:
-                dx, dy = -1, 0
-            elif key == K_RIGHT:
-                dx, dy = 1, 0
-                
-    if not myname:  # do nothing until the server welcomed us
-        continue
+            elif key in valid_inputs:
+                msg = {'input': valid_inputs[key]}
+                client.do_send(msg)
     
     # draw everything
     screen.fill((0, 0, 64))  # dark blue
     [draw_rect(screen, (0, 191, 255), b) for b in borders]  # deep sky blue 
     [draw_rect(screen, (255, 192, 203), p) for p in pellets]  # shrimp
-    for name, hisbox in players.items():  # draw other players
-        if name != myname and hisbox:
-            if mybox and hisbox.width < mybox.width:
-                color = 0, 255, 0  # smaller than me: green
-            else:
-                color = 255, 0, 0  # bigger or same size: red
-            draw_rect(screen, color, hisbox)
-            # anti-aliased, black
-            text = font.render(str(hisbox.width), 1, (0, 0, 0))  
-            screen.blit(text, hisbox)
-    draw_rect(screen, (0, 191, 255), mybox)  # Deep Sky Blue
-    text = font.render(str(mybox.width), 1, (0, 0, 0))  # anti-alias, black
-    screen.blit(text, mybox)
-
-    update_display()
+    for name, p in players.items():
+        if name != myname:
+            draw_rect(screen, (255, 0, 0), p)  # red
+    if myname:
+        draw_rect(screen, (0, 191, 255), players[myname])  # deep sky blue
+    
+    update_pygame_display()
+    
+    clock.tick(50)  # frames per second, independent of server frame rate
--- server-master/server.py
+++ server-master/server.py
@@ -1,63 +1,127 @@
 """
 Server master:
 The server is almighty. 
-The server regularly sends the WHOLE game state to the clients.
-The clients do not interpolate player commands. 
+Every frame, it receives player inputs from clients,
+executes these inputs to update the game state,
+and sends the whole game state to all the clients for display. 
 """
+from network import Listener, Handler, poll
 from random import randint
 from time import sleep
-import time
-
-from network import Listener, Handler, poll
 
 
-# gameplay 
-pellets = []
-for _ in range(4):  # 4 pellets in the game
-    pellets.append([randint(10, 390), randint(10, 290), 5, 5])
-    
-players = {}  # map name to rectangle
+##################### game logic #############
+# game state
+borders = [[0, 0, 2, 300], [0, 0, 400, 2], [398, 0, 2, 300], [0, 298, 400, 2]]
+pellets = [[randint(10, 390), randint(10, 290), 5, 5] for _ in range(4)]
+players = {}  # map a client handler to a player object 
+
 player_id = 0
-def add_player():
+def generate_name():
     global player_id
     player_id += 1
-    players[str(player_id)] = [randint(0, 280), randint(0, 380), 10, 10]
     return str(player_id)
+    
+# map inputs received from clients to directions
+input_dir = {'up': [0, -1], 'down': [0, 1],
+             'left': [-1, 0], 'right': [1, 0]}
 
-# network 
+class Player:
+    
+    def __init__(self):
+        self.name = generate_name()
+        self.revive()
 
-handlers = set()
-def broadcast(msg):
-    copies = handlers.copy()  # avoid "Set changed size during iteration" error
-    [h.do_send(msg) for h in copies]
+    def revive(self):
+        self.box = [randint(10, 380), randint(10, 280), 10, 10]
+        self.dir = input_dir['down']  # original direction: downwards
+    
+    def change_dir(self, input):
+        self.dir = input_dir[input]
+        
+    def move(self):
+        self.box[0] += self.dir[0] * 2
+        self.box[1] += self.dir[1] * 2
+        
+    def grow(self, qty=2):
+        self.box[2] += qty
+        self.box[3] += qty
+
+def collide_boxes(box1, box2):
+    x1, y1, w1, h1 = box1
+    x2, y2, w2, h2 = box2
+    return x1 < x2 + w2 and y1 < y2 + h2 and x2 < x1 + w1 and y2 < y1 + h1
+     
+################### network ##############
+
+event_queue = []  # list of ('event', handler) 
+# 'event' can be 'quit', 'join', 'up', 'down', 'left', 'right'
 
 class MyHandler(Handler):
         
     def on_open(self):
-        self.myname = add_player()  # create a new player
-        handlers.add(self)
-        self.do_send({'msg_type': 'welcome',
-                      'name': self.myname,
-                      'players': players,
-                      'pellets': pellets})  # send him the whole game state
-        broadcast({'msg_type': 'join',
-                   'name': self.myname,
-                   'state': players[self.myname]})  # tell everyone about him
+        event_queue.append(('join', self))
         
     def on_close(self):
-        del players[self.myname]
-        handlers.remove(self)
+        event_queue.append(('quit', self))
         
     def on_msg(self, data):
-        msgtype = data['msg_type']
-        
-              
+        event_queue.append((data['input'], self))
+    
 class Server(Listener):
     handlerClass = MyHandler
 
 server = Server(8888)
 
-# loop
+######################### loop #######################
+
 while 1:
+    
+    # enqueue the player events received by the client handlers
     poll()
-    sleep(.05)  # seconds
+    
+    # apply events onto game state
+    for event, handler in event_queue: 
+        if event == 'quit':
+            del players[handler]
+        elif event == 'join':
+            players[handler] = Player()
+        else:  # movement input
+            players[handler].change_dir(event)
+    event_queue = []
+    
+    # move everyone and detect collisions
+    for player in players.values():  
+        player.move()
+        for border in borders:  # collision with borders
+            if collide_boxes(player.box, border):
+                player.revive()
+        for p in players.values():  # collision with other players
+             # only the player with lowest id of the pair detects the collision
+            if player.name < p.name and collide_boxes(player.box, p.box):
+                playerw, pw = player.box[2], p.box[2]  # widths
+                if playerw > pw:
+                    player.grow(pw)
+                    p.revive()
+                elif playerw < pw:
+                    p.grow(playerw)
+                    player.revive()
+                else:  # they have same width: kill both 
+                    p.revive()
+                    player.revive()
+        for index, pellet in enumerate(pellets):  # collision with pellets
+            if collide_boxes(player.box, pellet):
+                player.grow()
+                pellets[index] = [randint(10, 390), randint(10, 290), 5, 5]
+        
+    # Send to all players 1) the whole game state, and 2) their own name, 
+    # so each player can draw herself differently from the other players.
+    serialized_players = {p.name: p.box for p in players.values()}
+    for handler, player in players.items():
+        msg = {'borders': borders,
+               'pellets': pellets,
+               'myname': player.name,
+               'players': serialized_players}
+        handler.do_send(msg)
+        
+    sleep(1. / 20)  # seconds
