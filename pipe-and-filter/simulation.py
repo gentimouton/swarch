3192b4615b45528326a38ff3caca089d76fc770d
tho deb <gentimouton@gmail.com>
2014-04-10 18:53:04 -0700

    working pipe and filter with pygame and console displays

--- pipe-and-filter/simulation.py
+++ pipe-and-filter/simulation.py
@@ -1,10 +1,20 @@
-# input: stream of commands 
-# output: game state
-import sys
+"""
+input: stream of commands 
+output: game state
+
+One thread blocks, listening for inputs from stdin.
+The other thread updates the game simulation at regular intervals,
+and prints the game state in JSON:
+{'mybox': [1,2,3,4], 
+ 'borders': [ [1,2,3,4], [2,3,4,5] ], 
+ 'pellets': [ [1,2,3,4] ] }
+"""
 import json
 from random import randint
+import sys
+from threading import Thread
 from time import sleep
-from hmac import new
+
 
 dir_vectors = {'U': (0, -1), 'D': (0, 1), 'L': (-1, 0), 'R': (1, 0)}
 
@@ -12,7 +22,8 @@ dims = 300, 400
 borders = [[0, 0, 2, 300], [0, 0, 400, 2], [398, 0, 2, 300], [0, 298, 400, 2]]
 pellets = [ [randint(10, 380), randint(10, 280), 5, 5] for _ in range(4) ]
 mybox = [200, 150, 10, 10]  # start in middle of the screen
-mydir = dir_vectors['D']  # start direction: down 
+myvector = dir_vectors['D']  # start direction: down 
+game_over = False  # True when the pipe from inputs is closed 
 
 
 def collide_boxes(box1, box2):
@@ -22,40 +33,40 @@ def collide_boxes(box1, box2):
     
     
 def update_sim():
-    # move me
-    global mybox
-    mybox[0] += mydir[0]
-    mybox[1] += mydir[1]
-    # potential collision with a border
-    for b in borders:
+    global mybox  # move me
+    mybox[0] += myvector[0]
+    mybox[1] += myvector[1]
+    for b in borders:  # potential collision with a border
         if collide_boxes(mybox, b):
             mybox = [200, 150, 10, 10]
-    # potential collision with a pellet
-    for index, pellet in enumerate(pellets):
+    for index, pellet in enumerate(pellets):  # potential collision with pellet
         if collide_boxes(mybox, pellet):
             mybox[2] *= 1.2
             mybox[3] *= 1.2
             pellets[index] = [randint(10, 380), randint(10, 280), 5, 5]
-            
 
-# readline and rstrip: http://stackoverflow.com/a/2813530/856897
-while 1:
-    new_dir = sys.stdin.readline().rstrip()  # without trailing \n
-    
-    # new_dir = sys.stdin.read(1)
-    # TODO: currently blocking for input read
-    # use a Thread http://stackoverflow.com/a/4896288/856897
-    # or select/poll http://repolinux.wordpress.com/2012/10/09/non-blocking-read-from-stdin-in-python/
-    # AND replace readline by os.read() http://bugs.python.org/issue1175#msg56041
-    
-    if new_dir:
-        mydir = dir_vectors[new_dir]
-        update_sim()
-        game = {'borders': borders, 'pellets': pellets, 'mybox': mybox} 
-        print json.dumps(game)
-        sys.stdout.flush()  # don't buffer output
 
-    else:  # readline returns '' when I read EOF
-        exit()
-    # sleep(0.02) # in seconds
-        
+def read_stdin():
+    global myvector, game_over
+    while not game_over:
+        # readline and rstrip: http://stackoverflow.com/a/2813530/856897
+        new_dir = sys.stdin.readline().rstrip()  # blocking; remove trailing \n
+        if new_dir:
+            myvector = dir_vectors[new_dir]
+        else:  # readline returns '' when I read EOF, ie the pipe closed
+            game_over = True
+
+thread = Thread(target=read_stdin)
+thread.daemon = True  # die when the main thread dies 
+thread.start()
+# on non-Windows OS, I could also use select on stdin
+# http://repolinux.wordpress.com/2012/10/09/non-blocking-read-from-stdin-in-python/    
+
+
+while not game_over:
+    update_sim()
+    game = {'borders': borders, 'pellets': pellets, 'mybox': mybox} 
+    print json.dumps(game)
+    sys.stdout.flush()  # don't buffer output
+    sleep(0.02)  # in seconds
+    
